189beba3e84ccdb89a63bc023e7c405d3d4622a2
Jacen H <>
2024-09-25 22:33:56 +0800

    frontend: api methods

--- frontend/src/apis/toDoApi.tsx
+++ frontend/src/apis/toDoApi.tsx
@@ -17,6 +17,26 @@ export async function getToDoList(): Promise<toDoItemI[]> {
         .catch((err) => console.error("error:" + err));
 }
 
+export async function createToDoItem(name:string): Promise<toDoItemI[]> {
+    const url = `${DOMAIN}/`;
+    const options = {
+        method: "POST",
+        body: JSON.stringify(
+            {
+                name: name
+            }
+        ),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    return await fetch(url, options)
+        .then((res) => res.json())
+        .then((json) => json)
+        .catch((err) => console.error("error:" + err));
+}
+
 export async function updateToDoItem(id:string, name: string): Promise<toDoItemI[]> {
     const url = `${DOMAIN}/${id}`;
     const options = {
@@ -34,5 +54,16 @@ export async function updateToDoItem(id:string, name: string): Promise<toDoItemI
     return await fetch(url, options)
         .then((res) => res.json())
         .then((json) => json)
+        .catch((err) => console.error("error:" + err));
+}
+
+export async function deleteToDoItem(id:string) {
+    const url = `${DOMAIN}/${id}`;
+    const options = {
+        method: "DELETE",
+    };
+
+    return await fetch(url, options)
+        .then((res) => res)
         .catch((err) => console.error("error:" + err));
 }
