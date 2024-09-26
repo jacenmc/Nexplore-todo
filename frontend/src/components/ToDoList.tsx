01c7b6000e29a97f8faaa99d7e702b7330db3aef
Jacen H <>
2024-09-26 16:23:02 +0800

    frontend: set loading logic prevent double click

--- frontend/src/components/ToDoList.tsx
+++ frontend/src/components/ToDoList.tsx
@@ -10,26 +10,39 @@ import ToDoItem from "./ToDoItem";
 
 function ToDoList() {
     const [toDoList, setToDoList] = useState<toDoItemI[]>([]);
+    const [loading, setLoading] = useState<boolean>(false);
 
-    const fetchList = async () => {
+    const fetchList = async (firstcall:boolean = true) => {
+        if(firstcall){
+            setLoading(true);
+        }
         const data = await getToDoList();
         data.sort((a, b) => Number(a.id) - Number(b.id));
         setToDoList(data);
+        if(firstcall){
+            setLoading(false);
+        }
     };
 
     const updateItem = async (id: string, name: string) => {
-        const data = await updateToDoItem(id, name);
-        fetchList();
+        setLoading(true);
+        await updateToDoItem(id, name);
+        await fetchList(false);
+        setLoading(false);
     };
 
     const addNewField = async () => {
+        setLoading(true);
         await createToDoItem("");
-        fetchList();
+        await fetchList(false);
+        setLoading(false);
     };
 
     const deleteItem = async (id: string) => {
+        setLoading(true);
         await deleteToDoItem(id);
-        fetchList();
+        await fetchList(false);
+        setLoading(false);
     };
 
     useEffect(() => {
@@ -37,7 +50,7 @@ function ToDoList() {
     }, []);
 
     return (
-        <div className="w-full max-w-[400px] p-4">
+        <div className={`w-full max-w-[400px] p-4 ${loading ? "pointer-events-none opacity-20" : ""}`}>
             {toDoList.map((e) => {
                 return (
                     <ToDoItem
